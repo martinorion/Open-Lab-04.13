05dc58e1b16755f6c94908a1b1642a6cd6cd8e89
martinorion <71751917+martinorion@users.noreply.github.com>
2021-01-15 18:51:39 +0100

    Return longest common substring of two strings

--- Open-Lab-04.13/StringTools.cs
+++ Open-Lab-04.13/StringTools.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._13
 {
@@ -6,7 +6,22 @@ namespace Open_Lab_04._13
     {
         public string GetLongestCommonSequence(string str1, string str2)
         {
-            throw new NotImplementedException();
+            string ret = "";
+            string compare = "";
+            for (int i = 0; i < str1.Length; i++)
+            {
+                for (int y = 0; y < str1.Length - i; y++)
+                {
+                    compare = str1.Substring(i, y);
+                    if (str2.Contains(compare))
+                    {
+                        if (compare.Length > ret.Length)
+                            ret = compare;
+                    }
+                }
+            }
+
+            return ret;
         }
     }
 }
